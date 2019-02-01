dfb8623d7a8a8573a7c2b3324491090a5bdd80ba
Lizzie Chan <lchan217@gmail.com>
2019-02-01 00:25:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,11 @@
 def oxford_comma(array)
   if array.length == 1 
     "#{array[0]}"
-    elsif array.length == 2 
-      "#{array[0]} and #{array[1]}"
-    else
-      array.each_with_index |ingredient, index|
-        "#{ingredient[index]}," 
-      end
-      "#{array[0]}, and #{array[1]}"
-  end 
-end 
+  elsif array.length == 2
+    "#{array[0]} and #{array[1]}"
+  else 
+  last = array[-1]
+  array.pop
+  array.join(", ") + ", and #{last}"
+  end
+end

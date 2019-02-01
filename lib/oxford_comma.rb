8c03da7ce4a9b63c71038d284b23c572af4e7ce1
Lizzie Chan <lchan217@gmail.com>
2019-02-01 00:23:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,9 @@ def oxford_comma(array)
     "#{array[0]}"
     elsif array.length == 2 
       "#{array[0]} and #{array[1]}"
-    else 
+    else
+      array.each_with_index |ingredient, index|
+        "#{ingredient[i]}" + "#{array[0]} and #{array[1]}"
       
   end 
 end 
